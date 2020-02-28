8bed1302750454ad835da868a2a94c5dcb238ff0
Haley-1617 <alicelai2920@gmail.com>
2020-02-27 17:40:20 -0800

    Initial Commit

---
+++ Sudoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sudoku
+//
+//  Created by Haley Lai on 2/27/20.
+//  Copyright © 2020 Haley Lai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
